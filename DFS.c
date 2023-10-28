3f18811be66b471755aa8f19a292518dc193bfce
Krish Panchal <kp1762004@gmail.com>
2023-10-28 16:31:17 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,49 +1,91 @@
 #include <stdio.h>
-int numVertices;
-int adjacencyList[100][100];
+#include <stdlib.h>
 
-void initializeGraph() {
-    for (int i = 0; i < numVertices; i++) {
-        for (int j = 0; j < numVertices; j++) {
-            adjacencyList[i][j] = 0;
-        }
+struct node {
+  int vertex;
+  struct node* next;
+};
+
+struct node* createNode(int v);
+
+struct Graph {
+  int numVertices;
+  int* visited;
+  struct node** adjLists;
+};
+void DFS(struct Graph* graph, int vertex) {
+  struct node* adjList = graph->adjLists[vertex];
+  struct node* temp = adjList;
+
+  graph->visited[vertex] = 1;
+  printf("Visited %d \n", vertex);
+
+  while (temp != NULL) {
+    int connectedVertex = temp->vertex;
+
+    if (graph->visited[connectedVertex] == 0) {
+      DFS(graph, connectedVertex);
     }
-}
-void addEdge(int vertex1, int vertex2) {
-    adjacencyList[vertex1][vertex2] = 1;
-    adjacencyList[vertex2][vertex1] = 1;
+    temp = temp->next;
+  }
 }
 
-void dfs(int vertex, int visited[]) {
-    visited[vertex] = 1;
-    printf("%d ", vertex);
+struct node* createNode(int v) {
+  struct node* newNode = malloc(sizeof(struct node));
+  newNode->vertex = v;
+  newNode->next = NULL;
+  return newNode;
+}
 
-    for (int i = 0; i < numVertices; i++) {
-        if (adjacencyList[vertex][i] == 1 && !visited[i]) {
-            dfs(i, visited);
-        }
+// Create graph
+struct Graph* createGraph(int vertices) {
+  struct Graph* graph = malloc(sizeof(struct Graph));
+  graph->numVertices = vertices;
+
+  graph->adjLists = malloc(vertices * sizeof(struct node*));
+
+  graph->visited = malloc(vertices * sizeof(int));
+
+  int i;
+  for (i = 0; i < vertices; i++) {
+    graph->adjLists[i] = NULL;
+    graph->visited[i] = 0;
+  }
+  return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest) {
+
+  struct node* newNode = createNode(dest);
+  newNode->next = graph->adjLists[src];
+  graph->adjLists[src] = newNode;
+  newNode = createNode(src);
+  newNode->next = graph->adjLists[dest];
+  graph->adjLists[dest] = newNode;
+}
+void printGraph(struct Graph* graph) {
+  int v;
+  for (v = 0; v < graph->numVertices; v++) {
+    struct node* temp = graph->adjLists[v];
+    printf("\n Adjacency list of vertex %d\n ", v);
+    while (temp) {
+      printf("%d -> ", temp->vertex);
+      temp = temp->next;
     }
+    printf("\n");
+  }
 }
 
 int main() {
-    numVertices = 7;
-    initializeGraph();
-    addEdge(0, 1);
-    addEdge(0, 2);
-    addEdge(1, 3);
-    addEdge(1, 4);
-    addEdge(2, 4);
-    addEdge(3, 5);
-    addEdge(4, 5);
+  struct Graph* graph = createGraph(4);
+  addEdge(graph, 0, 1);
+  addEdge(graph, 0, 2);
+  addEdge(graph, 1, 2);
+  addEdge(graph, 2, 3);
 
-    int visited[numVertices];
-    for (int i = 0; i < numVertices; i++) {
-        visited[i] = 0;
-    }
+  printGraph(graph);
 
-    printf("Depth First Search traversal of the graph: ");
-    dfs(0, visited);
-    printf("\n");
+  DFS(graph, 2);
 
-    return 0;
+  return 0;
 }
